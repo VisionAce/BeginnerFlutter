1145009d78420c4782c17a4a2f9ecf0ba2543f35
Jeff <ambitious4728@gmail.com>
2023-10-30 00:58:01 +0800

    Learn day2: NetworkImage&AssetImage

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'hello, jeff!',
-        style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey[600],
-            letterSpacing: 2.0,
-            fontFamily: 'IndieFlower'),
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('my first app'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(229, 57, 53, 1),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-      onPressed: () {},
-      backgroundColor: Colors.red[600],
-    ),
-  )));
+      body: Center(
+        // child: Image(
+        //     image: NetworkImage(
+        //       'https://images.unsplash.com/photo-1501549538842-2f24e2dd6520?ixlib=rb-1.2.1&ixid=eyJhcHBfawQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'),
+        //          AssetImage('assets/space-3.jpg')),
+        child: Image.asset('assets/space-2.jpg'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+        child: const Text('click'),
+      ),
+    );
+  }
 }
