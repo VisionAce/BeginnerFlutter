5bdb079ec1cffe704f51b5907200f391fc7d432d
Jeff <ambitious4728@gmail.com>
2023-10-31 20:47:51 +0800

    Learn day4: Flutter Tutorial for Beginners #10-12

---
+++ day-004/README.md
@@ -0,0 +1,68 @@
+# Day 4: Project 1: hello_world
+
+Follow along at 
+[Flutter Tutorial for Beginners #10 - Containers & Padding][1]
+[Flutter Tutorial for Beginners #11 - Rows][2]
+[Flutter Tutorial for Beginners #12 - Columns][3]
+
+# 📒 Field Notes
+It focuses on several specific topics:
+- Containers & Padding
+- Buttons
+
+## Containers & Padding
+
+- EdgeInsets.all
+    body: Container(
+           padding: EdgeInsets.all(20.0),
+           color: Colors.grey[400],
+           child: Text('Hello'),
+         ),
+- EdgeInsets.symmetric
+   padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+
+- EdgeInsets.fromLTRB
+   EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+
+- margin:
+     body: Container(
+           padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+           margin: EdgeInsets.all(30.0),
+           color: Colors.grey[400],
+           child: Text('Hello'),
+         ),
+
+- Padding()
+   body: Padding(
+           padding: EdgeInsets.all(90.0),
+           child: Text('Hello'),
+         ),
+
+## Rows
+
+   body: Row(
+           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: <Widget>[
+             Text('Hello, world'),
+             TextButton(
+               onPressed: () {},
+               style: TextButton.styleFrom(backgroundColor: Colors.amber),
+               child: Text('click me'),
+             ),
+             Container(
+               color: Colors.cyan,
+               padding: EdgeInsets.all(30.0),
+               child: Text('inside container'),
+             ),
+           ],
+         ),
+
+## Columns
+
+[Code Day4 commit][4]
+
+[1]: https://www.youtube.com/watch?v=H0cJ0gUlgE8&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ&index=11
+[2]: https://www.youtube.com/watch?v=a6oKFvGuTH4&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ&index=11
+[3]: https://www.youtube.com/watch?v=siFU8c_Heu0&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ&index=12
+[4]: 
--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -17,43 +17,34 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color.fromRGBO(229, 57, 53, 1),
       ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                    foregroundColor: const Color.fromARGB(255, 22, 27, 3),
-                    backgroundColor: Colors.amber),
-                onPressed: () {
-                  print('ElevatedButton: you clicked me');
-                },
-                icon: const Icon(Icons.mail),
-                label: const Text('mail'),
-              ),
-
-              const SizedBox(height: 20), // 用於在按鈕之間增加間距
-
-              TextButton(
-                onPressed: () {
-                  print('TextButton: you clicked me');
-                },
-                child: const Icon(
-                  Icons.grade,
-                  color: Colors.amber,
-                  size: 100.0,
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              OutlinedButton(
-                onPressed: () {
-                  print('OutlinedButton: you clicked me');
-                },
-                child: const Text('OutLined'),
-              ),
-            ]),
+              Text('Hello'),
+              Text('World!'),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.cyan,
+            child: Text('one'),
+          ),
+          Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.pinkAccent,
+            child: Text('two'),
+          ),
+          Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.amber,
+            child: Text('three'),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
